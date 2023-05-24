2bd65ed8eb742f736a6ce08c41dea37bc8e0627e
Qazi-Aimal <aimalqazi32@gmail.com>
2023-05-24 17:11:23 +0500

    adding data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  text: string;
+  completed: boolean;
+  isEditing: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular2';
+  todos: Todo[] = [];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push({
+        text: this.newTodo,
+        completed: false,
+        isEditing: false,
+      });
+      this.newTodo = '';
+    }
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  toggleComplete(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  editTodo(index: number) {
+    this.todos[index].isEditing = true;
+  }
+
+  updateTodo() {
+    this.todos.forEach((todo) => (todo.isEditing = false));
+  }
 }
